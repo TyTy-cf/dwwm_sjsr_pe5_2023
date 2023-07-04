52281b0f28fc43464936931d4c8378e2083e677f
Kevin Tourret <kevin@drosalys.fr>
2023-07-04 18:03:41 +0200

    correction exo J2 Symfony

---
+++ src/Controller/Front/GameController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Repository\GameRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/game', name: 'app_game_')]
+class GameController extends AbstractController
+{
+
+    public function __construct(
+        private GameRepository $gameRepository
+    ) { }
+
+    /**
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    #[Route('/{slug}', name: 'show')]
+    public function show(string $slug): Response
+    {
+        $game = $this->gameRepository->findFullOneBy($slug);
+
+        if ($game === null) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $relatedGames = $this->gameRepository->findByRelatedCategory($game, 9);
+
+        return $this->render('front/pages/game/show.html.twig', [
+            'game' => $game,
+            'relatedGames' => $relatedGames,
+        ]);
+    }
+
+}
--- src/Repository/GameRepository.php
+++ src/Repository/GameRepository.php
@@ -7,6 +7,7 @@ use App\Entity\Game;
 use App\Entity\UserOwnGame;
 use DateTime;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -72,6 +73,19 @@ class GameRepository extends ServiceEntityRepository
             ->getResult();
     }
 
+    public function findByRelatedCategory(Game $game, ?int $limit = null): array {
+        return $this->createQueryBuilder('g')
+            ->join('g.categories', 'c')
+            ->where('c IN (:categories)')
+            ->setParameter('categories', $game->getCategories())
+            ->andWhere('g != :currentGame')
+            ->setParameter('currentGame', $game)
+            ->setMaxResults($limit)
+            ->distinct(true) // Permet de bien prendre en compte la limite et d'ignorer le nombre instances
+            ->getQuery()
+            ->getResult();
+    }
+
 //        SELECT *
 //        FROM game
 //        JOIN game_category ON game_category.game_id = game.id
@@ -84,6 +98,22 @@ class GameRepository extends ServiceEntityRepository
             ->setParameter('categ', $category)
             ->getQuery()
             ->getResult();
+    }
+
+    /**
+     * @throws NonUniqueResultException
+     */
+    public function findFullOneBy(string $slug): ?Game {
+        return $this->createQueryBuilder('g')
+            ->select('g', 'categ', 'r', 'c', 'u')
+            ->leftJoin('g.categories', 'categ')
+            ->leftJoin('g.reviews', 'r')
+            ->leftJoin('r.user', 'u')
+            ->leftJoin('g.countries', 'c')
+            ->where('g.slug = :slug')
+            ->setParameter('slug', $slug)
+            ->getQuery()
+            ->getOneOrNullResult();
     }
 
 }
