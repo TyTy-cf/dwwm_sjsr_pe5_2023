d8c83087af9fc51ffb43af8e60271a49aa4a28d4
Kevin Tourret <kevin@drosalys.fr>
2023-07-05 12:26:59 +0200

    fix route issue

--- src/Controller/Front/GameController.php
+++ src/Controller/Front/GameController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller\Front;
 
+use App\Entity\Category;
+use App\Entity\Game;
+use App\Repository\CategoryRepository;
 use App\Repository\GameRepository;
 use Doctrine\ORM\NonUniqueResultException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,37 +12,52 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
-#[Route('/game', name: 'app_game_')]
+#[Route('/jeu', name: 'app_game_')]
 class GameController extends AbstractController
 {
 
     public function __construct(
         private GameRepository $gameRepository,
-        private TranslatorInterface $translator
+        private TranslatorInterface $translator,
+        private CategoryRepository $categoryRepository
     ) { }
 
     /**
      * @throws NonUniqueResultException
      */
-    #[Route('/{slug}', name: 'show')]
-    public function show(string $slug): Response
-    {
+    #[Route('/{slug}', name: 'redirect')]
+    public function handleRedirection(string $slug): Response {
         $game = $this->gameRepository->findFullOneBy($slug);
 
-        if ($game === null) {
-            $this->addFlash(
-                'danger',
-                $this->translator->trans('pages.game.show.error')
-            );
-            return $this->redirectToRoute('app_home');
+        if ($game !== null) {
+            return $this->show($game);
         }
 
+        $category = $this->categoryRepository->findOneBy(['slug' => $slug]);
+
+        if ($category !== null) {
+            return $this->listByCategory($category);
+        }
+
+        $this->addFlash(
+            'danger',
+            $this->translator->trans('pages.game.show.error')
+        );
+        return $this->redirectToRoute('app_home');
+    }
+
+    private function show(Game $game): Response
+    {
         $relatedGames = $this->gameRepository->findByRelatedCategory($game, 9);
 
         return $this->render('front/pages/game/show.html.twig', [
             'game' => $game,
             'relatedGames' => $relatedGames,
         ]);
+    }
+
+    private function listByCategory(Category $category): Response {
+        dd($category);
     }
 
 }
