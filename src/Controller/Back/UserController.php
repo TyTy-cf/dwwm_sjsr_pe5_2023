70873e9938156734aee44982c5c5b70953d83ce7
Kevin Tourret <kevin@drosalys.fr>
2023-07-20 10:23:14 +0200

    add knp paginator example

--- src/Controller/Back/UserController.php
+++ src/Controller/Back/UserController.php
@@ -3,7 +3,9 @@
 namespace App\Controller\Back;
 
 use App\Repository\UserRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +13,22 @@ class UserController extends AbstractController
 {
 
     #[Route('/admin/utilisateurs', name: 'app_admin_user_index')]
-    public function home(UserRepository $userRepository): Response {
+    public function home(
+        UserRepository $userRepository,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response {
+
+        $users = $paginator->paginate(
+            $userRepository->getQb(),
+            $request->query->getInt('page', 1),
+            10
+        );
+
         return $this->render('back/pages/user/index.html.twig', [
-            'users' => $userRepository->findAllFull(),
+            'users' => $users,
         ]);
     }
 
 }
+
