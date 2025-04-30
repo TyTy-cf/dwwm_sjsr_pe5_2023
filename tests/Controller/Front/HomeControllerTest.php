ebc838815ab60765e6c605547496b0c145d97c74
Kévin Tourret <kevin@drosalys.fr>
2025-04-30 13:31:13 +0200

    add more test on home

--- tests/Controller/Front/HomeControllerTest.php
+++ tests/Controller/Front/HomeControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller\Front;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HomeControllerTest extends WebTestCase
@@ -14,4 +15,33 @@ class HomeControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Instant-Faking');
     }
+
+    public function testUserAdmin(): void
+    {
+        $client = static::createClient();
+
+        // Log an user
+        $user = $this->getUserRepository()->findOneBy(['email' => 'kevin@drosalys.fr']);
+        $client->loginUser($user);
+
+        $client->request('GET', '/');
+        $this->assertSelectorExists('a[href="/admin"]');
+    }
+
+    public function testUserNotAdmin(): void
+    {
+        $client = static::createClient();
+
+        // Log an user
+        $user = $this->getUserRepository()->findOneBy(['email' => 'zprosacco@hotmail.com']);
+        $client->loginUser($user);
+
+        $client->request('GET', '/');
+        $this->assertSelectorNotExists('a[href="/admin"]');
+    }
+
+    private function getUserRepository(): UserRepository {
+        return $this->getContainer()->get(UserRepository::class);
+    }
+
 }
--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -14,4 +14,42 @@ class SecurityControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Se connecter');
     }
+
+    public function testLoginOK(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/se-connecter');
+
+        // Get the form from the button "Se connecter"
+        $form = $crawler->selectButton('Se connecter')->form();
+
+        // Set the fields value ($form is an array, with key the input's name)
+        $form['email'] = 'kevin@drosalys.fr';
+        $form['password'] = '12345';
+
+        // Submit the form
+        $client->submit($form);
+
+        // On login success => redirect to "home"
+        $this->assertResponseRedirects('/');
+    }
+
+    public function testLoginKO(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/se-connecter');
+
+        // Get the form from the button "Se connecter"
+        $form = $crawler->selectButton('Se connecter')->form();
+
+        // Set the fields value ($form is an array, with key the input's name)
+        $form['email'] = 'kevin@drosalys.fr';
+        $form['password'] = '1234';
+
+        // Submit the form
+        $client->submit($form);
+
+        // On login success => redirect to "home"
+        $this->assertResponseRedirects('/se-connecter');
+    }
 }
